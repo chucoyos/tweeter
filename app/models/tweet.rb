9321de3762a0aa09ff2ce1ddae83affd40753f77
chucoyos <facturasmanzanillo@hotmail.com>
2021-07-20 01:52:54 -0500

    add edit link to tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ApplicationRecord
     after_initialize :set_defaults, unless: :persisted?
-  # The set_defaults will only work if the object is new
+    # The set_defaults will only work if the object is new
+    
+    validates :title, presence: true
 
     after_create_commit { broadcast_prepend_to 'tweets' }
     after_update_commit { broadcast_replace_to 'tweets' }
