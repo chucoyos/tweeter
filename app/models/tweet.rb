b8f485d77e1e0d736ed82ec7fb266b3a772df9fc
chucoyos <facturasmanzanillo@hotmail.com>
2021-07-19 23:36:57 -0500

    set hotwire code

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,13 @@
 class Tweet < ApplicationRecord
+    after_initialize :set_defaults, unless: :persisted?
+  # The set_defaults will only work if the object is new
+
+    after_create_commit { broadcast_prepend_to 'tweets' }
+    after_update_commit { broadcast_replace_to 'tweets' }
+    after_destroy_commit { broadcast_remove_to 'tweets' }
+
+    def set_defaults
+        self.likesCount = 0
+        self.retweetsCount = 0
+    end
 end
